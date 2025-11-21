0fa28736b6ec035c3dc7b2b157f6e75ac1fb2bd6
nasgunawann <nasgunawann@gmail.com>
2025-11-21 21:29:16 +0700

    change scroll margin supaya ga ketutup navbar

--- src/components/cta-banner.tsx
+++ src/components/cta-banner.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 export default function CTABanner() {
   return (
-    <div id="cta" className="px-6 scroll-mt-52">
+    <div id="cta" className="px-6 scroll-mt-38">
       <motion.div
         initial={{ opacity: 0, y: 30 }}
         whileInView={{ opacity: 1, y: 0 }}
